2f777a18c56558b29649a45ebd28e4273ccb435a
Gonçalo Pataca <goncalo.pataca@unit4.com>
2023-05-13 21:49:37 +0100

    Delete Feature Added

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ cats = [{'name': "Daisy", 'race': "Siamese", 'age': 4},
         {'name': "Bailey", 'race': "Siamese", 'age': 2},
         {'name': "Babie", 'race': "British Shorthair", 'age': 3}]
 
-
-@app.route('/kats')
+@app.route('/cats')
 def getAllCats():
     return cats
+
+@app.route('/cats', methods=['DELETE'])
+def deletelist():
+    global cats 
+    cats = []  
+    return "List deleted"
+
 
 
 if __name__ == '__main__':
