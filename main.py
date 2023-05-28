85f16e90ec345841f708fae9f0bf7c655b46df25
Gonçalo Pataca <goncalo.pataca@unit4.com>
2023-05-28 20:18:42 +0100

    Cats API fully functional

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -12,32 +12,52 @@ def getAllCats():
     return cats
 
 
-@app.route('/cats/<id>')
+@app.route('/cats/<int:id>')
 def getCatByID(id):
-    id = int(id)
-    global cats
     for cat in cats:
         if cat['id'] == id:
             return cat
     return "No cats were found"
 
 
+@app.route('/cats', methods=['POST'])
+def AddCat():
+    json_data = request.get_json()
+    for cat in cats:
+        if cat['id'] == json_data['id']:
+            return "This cat is already added in the list"
+    cats.append(json_data)
+
+    return 'Cat added'
+
+
 @app.route('/cats', methods=['DELETE'])
 def deletelist():
-    global cats
-    cats = []
+    cats.clear()
     return "List deleted"
 
 
-@app.route('/cats/<id>', methods=['DELETE'])
+@app.route('/cats/<int:id>', methods=['DELETE'])
 def deleteCatByID(id):
-    id = int(id)
-    global cats
     for cat in cats:
         if cat['id'] == id:
             cats.remove(cat)
             return "Cat was successfully removed"
     return "This cat ID either doesn't exist or was already deleted"
+
+
+@app.route('/cats/<int:id>', methods=['PUT'])
+def updateCatByID(id):
+    json_data = request.get_json()
+    for cat in cats:
+        if cat['id'] == id:
+            for cat2 in cats:
+                if cat2['id'] == json_data['id'] and cat2 != cat:
+                    return "This cat's ID is conflicting with an already existing one"
+            cats.remove(cat)
+            cats.append(json_data)
+            return "Cat updated"
+    return "This cat ID either doesn't exist or was deleted"
 
 
 if __name__ == '__main__':
